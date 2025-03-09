18c534be2af07c5ab023429e6ca58c49cd585e2a
Mike DiGioia <digioia.mike@gmail.com>
2025-03-09 19:03:47 -0400

    remove over done references

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 import process from "node:process";
-import * as path from "https://deno.land/std@0.207.0/path/mod.ts";
 
 import "jsr:@std/dotenv/load";
 
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: "static",
   vite: {
     // @ts-expect-error: tailwindcss is throwing an error
     plugins: [tailwindcss()],
     define: {
       "process.env": {
-        SANITY_STUDIO_PROJECT_ID: process.env['SANITY_STUDIO_PROJECT_ID'],
-        SANITY_STUDIO_DATASET: process.env['SANITY_STUDIO_DATASET'],
-      },
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src")
+        SANITY_STUDIO_PROJECT_ID: process.env["SANITY_STUDIO_PROJECT_ID"],
+        SANITY_STUDIO_DATASET: process.env["SANITY_STUDIO_DATASET"],
       },
     },
   },
   integrations: [
+    // @ts-ignore: this throwing error on some lsps
     sanity({
-      projectId: process.env['SANITY_STUDIO_PROJECT_ID'],
-      dataset: process.env['SANITY_STUDIO_DATASET'],
+      projectId: process.env["SANITY_STUDIO_PROJECT_ID"],
+      dataset: process.env["SANITY_STUDIO_DATASET"],
       // Set useCdn to false if you're building statically.
       useCdn: false,
       // Access the Studio on your.url/admin
-      studioBasePath: '/admin',
+      studioBasePath: "/admin",
       stega: {
-        studioUrl: '/admin'
-      }
+        studioUrl: "/admin",
+      },
     }),
     react(),
   ],
